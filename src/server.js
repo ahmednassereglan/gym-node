0131650345665e36ee5f30ea7033c7bfa37cacf6
ahmednassereglan <ahmednassereglan@gmail.com>
2022-10-18 19:35:41 +0200

    Editing

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+
+// App Config
 const app = express()
-const port = 8000
+dotenv.config()
 
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
 
